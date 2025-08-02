78e078aacfd172c1ab1f3e4beb82e4de553c72d9
Irtiza <a93246176@gmail.com>
2025-08-02 19:24:37 +0500

    add dep astro-sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://irtiza90.github.io',
+  
   vite: {
     plugins: []
-  }
+  },
+
+  integrations: [sitemap()]
 });
